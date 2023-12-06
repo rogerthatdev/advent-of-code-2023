2885f55946d80b1b25b8de33e15e373bca1f0852
Roger Martinez <roger.that.dev@gmail.com>
2023-12-05 23:24:58 -0800

    parsed input

--- 02/main.js
+++ 02/main.js
@@ -5,37 +5,32 @@ async function main() {
     const parseInput = async input => {
         try {
             const rawData = await fs.readFile(input, 'utf8');
-
-            // Helpers
-            const splitByLine = x => x.split('\n')
-
-            // example in: '9 green, 2 blue, 12 red'
-            // example out: {green: 9, blue: 2, red: 12}
-            const stringToObject = x => {
-                const anArray = x => x.split(', ') 
-                let obj = {};
-                anArray(x).forEach(item => {
-                    let [value, key] = item.split(' ')
-                    obj[key] = value;
-                })
-                return obj
+            const cubeSetObjectify = set => {
+                const setArray = set.split(', ')
+                const objectifySetArray = setArray => {
+                    let obj = {};
+                    setArray.forEach(item => {
+                        let [value, key] = item.split(' ')
+                        obj[key] = parseInt(value);
+                    })
+                    return obj
+                }
+                return objectifySetArray(setArray)
             }
-
-
-            const lineToMap = x => {
+            const gameArrayToMap = gameArray => {
                 let newMap = new Map();
-                const gameTitle = x.split(':')[0];
-
-                const gameMatches = x.split(': ')[1].split('; ').map(x=> stringToObject(x));
-                newMap.set(gameTitle, gameMatches)
-                return newMap;
+                gameArray.forEach(game => {
+                    const gameTitle = game.split(':')[0];
+                    const gameMatches = game.split(': ')[1].split('; ').map(x=> cubeSetObjectify(x));
+                    newMap.set(gameTitle, gameMatches)
+                })
+                return newMap
             }
+ 
 
-            console.log(lineToMap("Game 100: 9 green, 2 blue, 12 red; 2 blue, 14 red, 2 green; 14 red, 12 green"))
-
-
-            // console.log(splitByLine(rawData))
-
+            const parsed = gameArrayToMap(rawData.split('\n'))
+            return parsed
+         
 
         } catch (err) {
             console.error(err);
@@ -46,7 +41,7 @@ async function main() {
 
 main()
 .then((data) => {
-    console.log(data);
+    console.log(data.get('Game 1'));
 })
 .catch((err) => {
         console.error(err);
